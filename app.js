9ca3a2e4f0f9f231044d58d93b16045bd088a37f
Mufaddal007 <mufaddaldarbar16@gmail.com>
2025-01-19 17:13:30 +0530

    Episode_03  part 4

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
- const heading = <h1 id="heading">This is from jsx</h1>; 
- const root = ReactDOM.createRoot(document.getElementById("root")); 
- root.render(heading); 
- 
+// React Element 
+
+const  heading = <h1> helo world </h1> ; 
+
+
+// React component 
+const HeadingComponent = () => {
+        
+        return  (<div>
+            <Title />
+            <h1> functional component </h1>; 
+        </div> )
+}
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+
+const Title = () => {
+    return <h1>
+        Namaste react using Jsx title
+    </h1>
+}
+
+// Rending a component inside html
+root.render(<HeadingComponent />); 
