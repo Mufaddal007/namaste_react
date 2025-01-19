2383168672283f035fa5c84bacd5252f65110dbf
Mufaddal007 <mufaddaldarbar16@gmail.com>
2025-01-19 16:57:10 +0530

    Episode_03 part 4 jsx

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-const parent = React.createElement("div", {}, 
-   [
-    React.createElement("div", {}, 
-        [ 
-         React.createElement("h1", {}, "I am an h1 tag"), 
-         React.createElement("h2", {}, "I am an h2 tag")
-        ]),
-
-        React.createElement("div", {}, 
-            [ 
-             React.createElement("h1", {}, "I am an h1 tag"), 
-             React.createElement("h2", {}, "I am an h2 tag")
-            ]  )
-
-   ]    
-    )
+ const heading = <h1 id="heading">This is from jsx</h1>; 
+ const root = ReactDOM.createRoot(document.getElementById("root")); 
+ root.render(heading); 
  
-
-root.render(parent);
