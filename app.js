1442b29889be6f3092914770d516a8f040c0bb05
Mufaddal Darbar <mufaddaldarbar16@gmail.com>
2025-01-19 10:20:34 +0530

    Episode_01 part 4

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const heading = React.createElement("h1", {
-    id:"heading", 
-}, "Hello world from React!");
-const root = ReactDOM.createRoot(document.getElementById("root")); 
-root.render(heading); 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const parent = React.createElement("div", {}, 
+   [
+    React.createElement("div", {}, 
+        [ 
+         React.createElement("h1", {}, "I am an h1 tag"), 
+         React.createElement("h2", {}, "I am an h2 tag")
+        ]),
+
+        React.createElement("div", {}, 
+            [ 
+             React.createElement("h1", {}, "I am an h1 tag"), 
+             React.createElement("h2", {}, "I am an h2 tag")
+            ]  )
+
+   ]    
+    )
+ 
+
+root.render(parent);
 
