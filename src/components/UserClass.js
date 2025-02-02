5ed1e0fb8ac48641ef9962ac616b15d89c027dfd
Mufaddal007 <mufaddaldarbar16@gmail.com>
2025-02-02 13:32:54 +0530

    Episode_08 part 5

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,6 +1,7 @@
 import React from "react"; 
 class UserClass extends React.Component{ 
     constructor(props){
+        console.log("constructor called")
         super(props); 
         this.state = {
          count : 10, 
@@ -8,7 +9,11 @@ class UserClass extends React.Component{
         }
 
     }
+    componentDidMount(){
+        console.log("Component Did Mount")
+    }
     render(){
+        console.log("render  method")
         return (
             <div>This is class component
                 <h1>{this.props.name}</h1>
